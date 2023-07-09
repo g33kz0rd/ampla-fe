4eed1bb31b6ffe706b81196b2402f14f2d5bd43a
g33kz0rd <g33kz0rd@gmail.com>
2023-07-09 02:35:35 -0300

    Add share

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Sheet } from "./components/Sheet";
 import { DB } from "./types/db";
 
 function App() {
   const [db, setDB] = useState<DB>({});
+  const [shareLink, setShareLink] = useState<string>();
+
+  useEffect(() => loadLink(window.location.pathname.slice(1)), []);
+
+  const handleShare = () => setShareLink(generateLink());
+
+  const generateLink = (): string => {
+    return btoa(JSON.stringify(db));
+  };
+
+  const loadLink = (base64: string) => {
+    try {
+      setDB(JSON.parse(atob(base64)));
+    } catch (e) {}
+  };
 
   return (
     <div style={{ fontFamily: "monospace", fontSize: "large" }}>
+      <button onClick={handleShare}>Share!</button>
+      {!!shareLink && <a href={shareLink}>{shareLink}</a>}
       <Sheet
         rowCount={100}
         columnCount={30}
